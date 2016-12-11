cd940de950f4283ef64ba6bab9302cb1edd06622
Ruben Gallegos <gallegos_ruben@yahoo.com>
2016-12-10 21:35:01 -0700

    Added two types of enemies, the shambler and the hoverer. The two enemies have different types of AI, and come in endless waves of 5. An enemy controller coordinates the spawns and manages the enemy pool. A health display has been added as well. Death is possible, although control is still available.
    Next is adding more enemies, power ups, and scoring.

--- game.js
+++ game.js
@@ -6,9 +6,243 @@ window.onload = function() {
 
     var Enemy = {};
 
-    Enemy.
+    function getSpawnpoint() {
+        result = {};
+        //get a number 0-3
+        var side = Math.floor(Math.random() * 4);
+        var x, y;
+        if (side < 2) {
+            if (side === 0) {
+                //top
+                y = 80;
+            } else {
+                //bottom
+                y = game.world.height - 120;
+            }
+            x = Math.floor(Math.random()*600) + 40;
 
-    var Bullet = function (game, key) {
+        } else {
+            if (side === 2) {
+                //left
+                x = 80;
+            } else {
+                //right
+                x = game.world.width - 120;
+            }
+            y = Math.floor(Math.random()*400) + 40;
+
+        }
+        result.x = x;
+        result.y = y;
+        return result;
+    }
+
+    Enemy.Controller = function(game) {
+
+        this.game = game;
+        this.shamblers = [];
+        for (var i = 0; i < 20; i++) {
+            this.shamblers.push(new Enemy.Shambler(game, this));
+        }
+        this.hoverers = [];
+        for (var i = 0; i < 10; i++) {
+          this.hoverers.push(new Enemy.Hoverer(game, this));
+        }
+        this.drifters = [];
+        this.activeEnemies = [];
+        this.player = game.player;
+
+
+
+    };
+
+    Enemy.Controller.prototype.update = function () {
+
+        if (this.activeEnemies.length === 0) {
+            for (var i = 0; i < 5; i++) {
+              var pick = Math.floor(Math.random()*6);
+              if (pick < 4) {
+                  var shambler = this.shamblers.pop();
+                  shambler.spawn();
+                  this.activeEnemies.push(shambler);
+              } else {
+                  var hoverer = this.hoverers.pop();
+                  hoverer.spawn();
+                  this.activeEnemies.push(hoverer);
+              }
+            }
+          console.dir(this.activeEnemies);
+        }
+        this.activeEnemies.forEach(function(enemy) {
+            enemy.update();
+        });
+
+    };
+
+    Enemy.Controller.prototype.cleanUpEnemy = function (enemy) {
+        console.log(enemy.type + " died!");
+        enemy.controller.activeEnemies.splice(enemy.controller.activeEnemies.indexOf(enemy), 1);
+        enemy.enemy.exists = false;
+        if (enemy.type === 'shambler') enemy.controller.shamblers.push(enemy);
+        else if (enemy.type === 'hoverer') enemy.controller.hoverers.push(enemy);
+        else if (enemy.type === 'drifter') enemy.controller.drifters.push(enemy);
+        console.log(enemy.controller.activeEnemies.length, enemy.controller.shamblers.length, enemy.controller.hoverers.length, enemy.controller.drifters.length);
+    };
+
+    Enemy.Hoverer = function (game, controller) {
+
+        this.game = game;
+        this.controller = controller;
+        this.health = 3;
+        this.player = game.player;
+        this.AITimer = 15;
+        this.type = 'hoverer';
+        this.weapon = new Weapon.EnemyBallBullet(game);
+        this.hovererDeath = new Phaser.Signal();
+
+        this.hovererDeath.add(controller.cleanUpEnemy, this);
+
+        this.enemy = game.add.sprite(-100, -100, 'hoverer');
+        this.enemy.enemyInfo = this;
+        this.enemy.exists = false;
+        this.enemy.anchor.set(0.5);
+
+        game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+
+    };
+
+    Enemy.Hoverer.prototype.spawn = function () {
+
+        this.health = 3;
+        this.enemy.alive = true;
+        this.enemy.exists = true;
+        this.enemy.visible = true;
+        this.AITimer = 15;
+
+        spawnpoint = getSpawnpoint();
+
+        this.enemy.x = spawnpoint.x;
+        this.enemy.y = spawnpoint.y;
+
+    };
+
+    Enemy.Hoverer.prototype.damage = function(damage) {
+        this.health -= damage;
+
+        if (this.health <= 0) {
+            this.enemy.kill();
+            this.hovererDeath.dispatch(this);
+
+            return true;
+        }
+
+        return false;
+    };
+
+    Enemy.Hoverer.prototype.update = function() {
+
+        game.physics.arcade.collide(this.enemy, this.game.walls);
+        game.physics.arcade.collide(this.player, this.enemy, this.game.onEnemyCollide);
+        game.physics.arcade.collide(this.enemy, this.game.weapons[0], this.game.bulletEnemyHandler);
+        game.physics.arcade.collide(this.weapon, this,game.walls, this.game.bulletWallHandler);
+        game.physics.arcade.collide(this.player, this.weapon, this.game.bulletPlayerHandler);
+
+        if (this.AITimer === 0) {
+            this.enemy.rotation = game.physics.arcade.angleBetween(this.enemy, this.player);
+
+            this.weapon.fire(this.enemy);
+
+            var movRot = this.enemy.rotation;
+            if (game.physics.arcade.distanceBetween(this.enemy, this.player) < 300) {
+                if (Math.random() < .5) {
+                    movRot += (.75 * Math.PI);
+                    if (movRot > Math.PI) movRot -= 2 * Math.PI;
+                } else {
+                    movRot -= (.75 * Math.PI);
+                    if (movRot < -1 * Math.PI) movRot += 2 * Math.PI;
+                }
+            } else {
+                if (Math.random() < .5) {
+                    movRot += (.25 * Math.PI);
+                    if (movRot > Math.PI) movRot -= 2 * Math.PI;
+                } else {
+                    movRot -= (.25 * Math.PI);
+                    if (movRot < Math.PI) movRot += 2 * Math.PI;
+                }
+            }
+            game.physics.arcade.velocityFromRotation(movRot, 75, this.enemy.body.velocity);
+
+            this.AITimer = 15;
+        } else {
+            this.AITimer -= 1;
+        }
+    };
+
+    Enemy.Shambler = function (game, controller) {
+
+        this.game = game;
+        this.controller = controller;
+        this.health = 5;
+        this.player = game.player;
+        this.AITimer = 15;
+        this.type = 'shambler';
+
+        this.shamblerDeath = new Phaser.Signal();
+        this.shamblerDeath.add(controller.cleanUpEnemy, this);
+
+        this.enemy = game.add.sprite(-100, -100, 'shambler');
+        this.enemy.enemyInfo = this;
+        this.enemy.exists = false;
+        this.enemy.anchor.set(0.5);
+
+        game.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+
+    };
+
+    Enemy.Shambler.prototype.spawn = function () {
+
+        this.health = 5;
+        this.enemy.alive = true;
+        this.enemy.exists = true;
+        this.enemy.visible = true;
+        this.AITimer = 15;
+
+        spawnpoint = getSpawnpoint();
+
+        this.enemy.x = spawnpoint.x;
+        this.enemy.y = spawnpoint.y;
+    };
+
+    Enemy.Shambler.prototype.damage = function(damage) {
+        this.health -= damage;
+
+        if (this.health <= 0) {
+            this.enemy.kill();
+            this.shamblerDeath.dispatch(this);
+
+            return true;
+        }
+
+        return false;
+
+    };
+
+    Enemy.Shambler.prototype.update = function() {
+
+      game.physics.arcade.collide(this.enemy, this.game.walls);
+      game.physics.arcade.collide(this.player, this.enemy, this.game.onEnemyCollide);
+      game.physics.arcade.collide(this.enemy, this.game.weapons[0], this.game.bulletEnemyHandler);
+
+      if (this.AITimer === 0) {
+          this.enemy.rotation = game.physics.arcade.moveToObject(this.enemy, this.player, 50);
+          this.AITimer = 15;
+      } else {
+        this.AITimer -= 1;
+      }
+
+    };
+
+    var Bullet = function (game, key, health) {
         //health of bullets == the damage the bullet inflicts
 
         Phaser.Sprite.call(this, game, 0, 0, key);
@@ -22,6 +256,8 @@ window.onload = function() {
         this.exists = false;
 
         this.tracking = false;
+
+        this.health = health;
 
     };
 
@@ -52,15 +288,15 @@ window.onload = function() {
         this.nextFire = 0;
         this.bulletSpeed = 500;
         this.fireRate = 200;
-        this.health = 1;
 
         for (var i = 0; i < 64; i++) {
-          this.add(new Bullet(game, 'pbullet'), true);
+            this.add(new Bullet(game, 'pbullet', 1), true);
         }
 
         return this;
 
     };
+
 
     Weapon.SimpleBullet.prototype = Object.create(Phaser.Group.prototype);
     Weapon.SimpleBullet.prototype.constructor = Weapon.SimpleBullet;
@@ -78,7 +314,36 @@ window.onload = function() {
       this.nextFire = this.game.time.time + this.fireRate;
     };
 
+    Weapon.EnemyBallBullet = function (game) {
 
+        Phaser.Group.call(this, game, game.world, 'Enemy Ball Bullet', false, true, Phaser.Physics.ARCADE);
+
+        this.nextFire = 0;
+        this.bulletSpeed = 200;
+        this.fireRate = 800;
+
+        for (var i = 0; i < 64; i++) {
+            this.add(new Bullet(game, 'eball', 2), true);
+        }
+
+        return this;
+    };
+
+    Weapon.EnemyBallBullet.prototype = Object.create(Phaser.Group.prototype);
+    Weapon.EnemyBallBullet.prototype.constructor = Weapon.EnemyBallBullet;
+
+    Weapon.EnemyBallBullet.prototype.fire = function(source) {
+
+        if (this.game.time.time < this.nextFire) {return;}
+
+        var offset = this.game.physics.arcade.velocityFromRotation(source.rotation, 15);
+        var x = source.x + offset.x;
+        var y = source.y + offset.y;
+
+        this.getFirstExists(false).fire(x, y, source.rotation, this.bulletSpeed);
+
+        this.nextFire = this.game.time.time + this.fireRate;
+    };
 
     var PhaserGame = function () {
 
@@ -89,6 +354,7 @@ window.onload = function() {
         this.enemies = null;
         this.cursors = null;
         this.speed = 100;
+        this.hpDisplay = null;
 
         this.walls = null;
 
@@ -102,6 +368,7 @@ window.onload = function() {
 
         init: function () {
             this.physics.startSystem(Phaser.Physics.ARCADE);
+            this.stage.backgroundColor = 0x777777;
         },
 
         preload: function () {
@@ -109,12 +376,16 @@ window.onload = function() {
             this.load.image('logo', 'phaser.png');
             //this.load.image('character', 'characterplaceholder.png');
             this.load.image('character', 'SRCharacter.png');
-            this.load.image('smlenemy', 'smallenemyplaceholder.png');
+            this.load.image('hoverer', 'SRHoverer.png');
+            this.load.image('shambler', 'SRshambler.png');
+            //this.load.image('smlenemy', 'smallenemyplaceholder.png');
+            this.load.image('drifter', 'SRdrifter.png');
             this.load.image('enemy', 'basicenemyplaceholder.png');
-            this.load.image('medenemy', 'mediumenemyplaceholder.png');
+            //this.load.image('medenemy', 'mediumenemyplaceholder.png');
             this.load.image('lrgenemy', 'largeenemyplaceholder.png');
             this.load.image('wall', 'wallplaceholder.png');
             this.load.image('pbullet', 'pbullet.png');
+            this.load.image('eball', 'eball.png');
 
         },
 
@@ -143,7 +414,7 @@ window.onload = function() {
                 yPos = yPos + 40;
             }
 
-            this.weapons.push(new Weapon.SimpleBullet(this.game));
+            this.weapons.push(new Weapon.SimpleBullet(this.game)); //0
 
             this.currentWeapon = 0;
 
@@ -156,32 +427,37 @@ window.onload = function() {
 
             this.physics.arcade.enable(this.player);
 
-            this.player.health = 3;
+            this.player.health = 5;
 
+            this.player.hurt = function (damage) {
+                this.health -= damage;
 
+                if (this.health < 1) {
+                    this.kill()
+                    return false;
+                } else return true;
+            }
 
-            //this.enemy = this.add.sprite(this.world.centerX + 80, this.world.centerY - 20, 'enemy');
-            //this.enemy.anchor.setTo(0.5, 0.5);
+            this.hpDisplay = this.add.text(20, 570, 'PLAYER HEALTH: *****');
 
-            //this.physics.arcade.enable(this.enemy);
-
-            //this.enemy.health = 3;
-            //this.enemy.AITimer = 0;
+            this.enemies = new Enemy.Controller(this);
 
             this.cursors = this.input.keyboard.createCursorKeys();
             this.cursors.pointer = this.input.activePointer;
-
-            console.dir(this.cursors);
         },
 
         update: function () {
 
             //collision
             var touchWall = this.physics.arcade.collide(this.player, this.walls);
-            //var enemyWall = this.physics.arcade.collide(this.enemy, this.walls);
-            //var touchEnemy = this.physics.arcade.collide(this.player, this.enemy, this.onEnemyCollide);
             var bulletWall = this.physics.arcade.collide(this.weapons[0], this.walls, this.bulletWallHandler);
-            //var bulletEnemy = this.physics.arcade.collide(this.weapons[0], this.enem, this.bulletEnemyHandler);
+
+            //health display
+            var hpText = 'PLAYER HEALTH: '
+            for (var i = 0; i < this.player.health; i++) {
+              hpText = hpText + '*'
+            }
+            this.hpDisplay.setText(hpText);
 
             //control
             this.player.body.velocity.set(0);
@@ -204,30 +480,26 @@ window.onload = function() {
             //rotation
             this.player.rotation = this.physics.arcade.angleToPointer(this.player);
 
-            //enemyAI
-            // if (this.enemy.AITimer === 0) {
-            //     this.enemy.rotation = this.physics.arcade.moveToObject(this.enemy, this.player, 50);
-            //     this.enemy.AITimer = 15;
-            // } else {
-            //   this.enemy.AITimer -= 1;
-            // }
+            //enemies
+            this.enemies.update();
 
         },
 
-        // onEnemyCollide: function (player, enemy) {
-        //
-        //     player.health -= 1;
-        //     enemy.body.velocity.x = 0;
-        //     enemy.body.velocity.y = 0;
-        //
-        //     var bump = game.physics.arcade.velocityFromRotation(enemy.rotation, 20);
-        //
-        //     if (player.x + bump.x > 50 && player.x + bump.x < game.world.width - 50) player.x += bump.x;
-        //     if (player.y + bump.y > 50 && player.y + bump.y < game.world.height - 50) player.y += bump.y;
-        //
-        //     if (enemy.x - bump.x > 50 && enemy.x - bump.x < game.world.width - 50) enemy.x -= bump.x;
-        //     if (enemy.y - bump.y > 50 && enemy.y - bump.y < game.world.height - 50) enemy.y -= bump.y;
-        // },
+        onEnemyCollide: function (player, enemy) {
+
+            player.hurt(1);
+
+            enemy.body.velocity.x = 0;
+            enemy.body.velocity.y = 0;
+
+            var bump = game.physics.arcade.velocityFromRotation(enemy.rotation, 20);
+
+            if (player.x + bump.x > 50 && player.x + bump.x < game.world.width - 50) player.x += bump.x;
+            if (player.y + bump.y > 50 && player.y + bump.y < game.world.height - 50) player.y += bump.y;
+
+            if (enemy.x - bump.x > 50 && enemy.x - bump.x < game.world.width - 50) enemy.x -= bump.x;
+            if (enemy.y - bump.y > 50 && enemy.y - bump.y < game.world.height - 50) enemy.y -= bump.y;
+        },
 
         bulletWallHandler: function (bullet, wall) {
 
@@ -235,26 +507,32 @@ window.onload = function() {
 
         },
 
-        // bulletEnemyHandler: function (enemy, bullet) {
-        //
-        //     enemy.health -= bullet.health;
-        //     bullet.kill();
-        //
-        //     if (enemy.health <= 0) {
-        //       enemy.kill();
-        //     }
-        //
-        //     enemy.body.velocity.x = 0;
-        //     enemy.body.velocity.y = 0;
-        //
-        //     var bump = game.physics.arcade.velocityFromRotation(bullet.rotation, 10);
-        //
-        //     if (enemy.x + bump.x > 50 && enemy.x + bump.x < game.world.width - 50) enemy.x += bump.x;
-        //     if (enemy.y + bump.y > 50 && enemy.y + bump.y < game.world.height - 50) enemy.y += bump.y;
-        //
-        //
-        //
-        // }
+        bulletEnemyHandler: function (enemy, bullet) {
+
+            bullet.kill();
+            if (!enemy.enemyInfo.damage(bullet.health)) {
+
+              enemy.body.velocity.x = 0;
+              enemy.body.velocity.y = 0;
+
+              var bump = game.physics.arcade.velocityFromRotation(bullet.rotation, 10);
+
+              if (enemy.x + bump.x > 50 && enemy.x + bump.x < game.world.width - 50) enemy.x += bump.x;
+              if (enemy.y + bump.y > 50 && enemy.y + bump.y < game.world.height - 50) enemy.y += bump.y;
+            }
+        },
+
+        bulletPlayerHandler: function (player, bullet) {
+
+            bullet.kill();
+
+            if (player.hurt(bullet.health)) {
+                var bump = game.physics.arcade.velocityFromRotation(bullet.rotation, 10);
+
+                if (player.x + bump.x > 50 && player.x + bump.x < game.world.width - 50) player.x += bump.x;
+                if (player.y + bump.y > 50 && player.y + bump.y < game.world.height - 50) player.y += bump.y;
+            }
+        }
 
     };
 
